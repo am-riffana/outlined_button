f9cde2371a64e3955a37e52467c1695bdd360ccf
rifana sherin <rifanasherin80@gmail.com>
2026-04-03 14:32:14 +0530

    outlined button

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:  MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title:  Text("OUTLINED BUTTON"),
+      ),
+      body: Center(
+        child: OutlinedButton(
+          onPressed: () {
+            print("Button click aayi");
+          },
+          style: OutlinedButton.styleFrom(
+            side:  BorderSide(color: Colors.blue, width: 2),
+            padding:  EdgeInsets.symmetric(horizontal: 21, vertical: 12),
+          ),
+          child:  Text(
+            "Retry",
+            style: TextStyle(fontSize: 18),
+          ),
+        ),
+      ),
+    );
+  }
+}
